d64bc0e729dbd52c412720b54429ea7c740c41c2
yumoqing <yumoqing@gmail.com>
2022-07-28 12:19:37 +0800

    bugfix

--- ios_tts/__init__.py
+++ ios_tts/__init__.py
@@ -1,66 +1,88 @@
-from pyobjus import autoclass
+from pyobjus import autoclass, protocol
 from pyobjus.dylib_manager import load_framework, INCLUDE
+load_framework(INCLUDE.AVFoundation)
 load_framework('/System/Library/Frameworks/AppKit.framework')
 load_framework('/System/Library/Frameworks/Foundation.framework')
-from PyObjCTools import AppHelper
-from ..voice import Voice
+from pyttsx3.voice import Voice
 NSTimer = autoclass('NSTimer')
 NSString = autoclass('NSString')
 NSArray = autoclass('NSArray')
 NSObject = autoclass('NSObject')
-NSSpeechSynthesizer = autoclass('NSSpeechSynthesizer')
+AVSpeechSynthesizer = autoclass('AVSpeechSynthesizer')
+AVSpeechUtterance = autoclass('AVSpeechUtterance')
+AVSpeechSynthesisVoice = autoclass('AVSpeechSynthesisVoice')
+NSURL = autoclass('NSURL')
 
+from text2sentences import text_to_sentences
 
+def buildDriver(proxy, **kw):
+    # return NSSpeechDriver.alloc().initWithProxy(proxy)
+    return SpeechDriver(proxy, **kw)
 
-def buildDriver(proxy):
-    return NSSpeechDriver.alloc().initWithProxy(proxy)
+class SpeechDriver:
+    def __init__(self, proxy, **kw):
+		self._proxy = proxy
+		self._tts = AVSpeechSynthesizer.alloc().init()
+		self._tts.setDelegate_(self)
+		self._completed = True
+		self.rate = 180
+		self.set_stop_period()
+		self.voice = None
 
+	def set_stop_period(self):
+		r = 1.0 / float(self.rate)
+		self.semi_stop_period = 1.5 * r
+		self.sentence_stop_period = 3.0 * r
 
-class NSSpeechDriver(NSObject):
-    @objc.python_method
-    def initWithProxy(self, proxy):
-        self = super(NSSpeechDriver, self).init()
-        if self:
-            self._proxy = proxy
-            self._tts = NSSpeechSynthesizer.alloc().initWithVoice_(None)
-            self._tts.setDelegate_(self)
-            # default rate
-            self._tts.setRate_(200)
-            self._completed = True
-        return self
+	def pause(self):
+		self._tts.pauseSpeakingAtBoundary()
+
+	def contineu(self):
+		self._tts.continueSpeaking()
+
+	def stop(self):
+        if self._proxy.isBusy():
+            self._completed = False
+		self._tts.stopSpeakingAtBoundary()
+
+	def say(self, text):
+        self._proxy.setBusy(True)
+        self._completed = True
+		self.sentences += text_to_sentences(text)
+
+	def set_utterences_by_sentence(self, utterence, sentence):
+		if sentence.dialog:
+			utterence.pitchModifier = 1.25
+
+	def speak_sentence(self, sentence):
+		utterance = AVSpeechUtterance.alloc().initWithString(sentence.text)
+		self.set_utterences_by_sentence(utterence, sentence)
+		self._tts.speakUtterance(utterance)
 
     def destroy(self):
         self._tts.setDelegate_(None)
         del self._tts
 
-    def onPumpFirst_(self, timer):
-        self._proxy.setBusy(False)
-
     def startLoop(self):
-        NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
-            0.0, self, 'onPumpFirst:', None, False)
-        AppHelper.runConsoleEventLoop()
+        self._proxy.notify('started-utterance')
+		speak_next_sentence()
+
+	def speak_next_sentence(self):
+		if len(self.sentences) > 0:
+			self.speaking_sentence = self.sentences.pop(0)
+			self.speak_sentence(self.speaking_sentence)
+		else:
+			sself.speaking_sentence = None
+			self._proxy.notify('finished-utterance', completed=success)
+			
 
     def endLoop(self):
-        AppHelper.stopEventLoop()
+		self.speaking_sentence = None
 
     def iterate(self):
         self._proxy.setBusy(False)
         yield
 
-    @objc.python_method
-    def say(self, text):
-        self._proxy.setBusy(True)
-        self._completed = True
-        self._proxy.notify('started-utterance')
-        self._tts.startSpeakingString_(text)
-
-    def stop(self):
-        if self._proxy.isBusy():
-            self._completed = False
-        self._tts.stopSpeaking()
-
-    @objc.python_method
     def _toVoice(self, attr):
         try:
             lang = attr['VoiceLocaleIdentifier']
@@ -70,31 +92,25 @@ class NSSpeechDriver(NSObject):
                      [lang], attr['VoiceGender'],
                      attr['VoiceAge'])
 
-    @objc.python_method
     def getProperty(self, name):
         if name == 'voices':
-            return [self._toVoice(NSSpeechSynthesizer.attributesForVoice_(v))
-                    for v in list(NSSpeechSynthesizer.availableVoices())]
+            return self.get_all_language()
         elif name == 'voice':
-            return self._tts.voice()
+            return self.voice
         elif name == 'rate':
-            return self._tts.rate()
+            return None
         elif name == 'volume':
-            return self._tts.volume()
+            return None
         elif name == "pitch":
             print("Pitch adjustment not supported when using NSSS")
         else:
             raise KeyError('unknown property %s' % name)
 
-    @objc.python_method
     def setProperty(self, name, value):
+		return
         if name == 'voice':
             # vol/rate gets reset, so store and restore it
-            vol = self._tts.volume()
-            rate = self._tts.rate()
-            self._tts.setVoice_(value)
-            self._tts.setRate_(rate)
-            self._tts.setVolume_(vol)
+			self.voice = value
         elif name == 'rate':
             self._tts.setRate_(value)
         elif name == 'volume':
@@ -104,19 +120,46 @@ class NSSpeechDriver(NSObject):
         else:
             raise KeyError('unknown property %s' % name)
 
-    @objc.python_method
+	def get_all_language(self):
+		langs = AVSpeechSynthesisVoice.speechVoices()
+		return langs
+
     def save_to_file(self, text, filename):
-        url = Foundation.NSURL.fileURLWithPath_(filename)
+        url = NSURL.fileURLWithPath_(filename)
         self._tts.startSpeakingString_toURL_(text, url)
 
-    def speechSynthesizer_didFinishSpeaking_(self, tts, success):
+	@protocol('AVSpeechSynthesisDelegate')
+    def speechSynthesizer_didCancelSpeechUtterance_(self, *args):
+		print('args=', args)
+		return
+
+	@protocol('AVSpeechSynthesisDelegate')
+    def speechSynthesizer_didContinueSpeechUtterance_(self, *args):
+		print('args=', args)
+		return
+
+	@protocol('AVSpeechSynthesisDelegate')
+    def speechSynthesizer_didPauseSpeechUtterance_(self, *args):
+		print('args=', args)
+		return
+
+	@protocol('AVSpeechSynthesisDelegate')
+    def speechSynthesizer_didStartSpeechUtterance_(self, *args):
+		print('args=', args)
+		return
+
+	@protocol('AVSpeechSynthesisDelegate')
+    def speechSynthesizer_didFinishSpeechUtterance_(self, *args):
+		print('args=', args)
+		self.speak_next_utterence()
+		return
         if not self._completed:
             success = False
         else:
             success = True
-        self._proxy.notify('finished-utterance', completed=success)
         self._proxy.setBusy(False)
 
-    def speechSynthesizer_willSpeakWord_ofString_(self, tts, rng, text):
-        self._proxy.notify('started-word', location=rng.location,
-                           length=rng.length)
+	@protocol('AVSpeechSynthesisDelegate')
+	def speechSynthesizer_willSpeakRangeOfSpeechString_(self, *args):
+		print('args=', args)
+
