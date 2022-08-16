35ca7b05759d60c3ad12638232813f466fdb761b
yumoqing <yumoqing@gmail.com>
2022-08-16 12:44:41 +0800

    bugfix

--- setup.py
+++ setup.py
@@ -3,29 +3,37 @@ try:
 except ImportError:
 	from distutils.core import setup
 
-from ios_tts.version import __version__
+# from ios_tts.version import __version__
 #with open('README.rst', 'r') as f:
 #	long_description = f.read()
+
+with open('ios_tts/version.py', 'r') as f:
+	x = f.read()
+	y = x[x.index("'")+1:]
+	z = y[:y.index("'")]
+	version = z
 
 setup(
 	name='ios_tts',
 	packages=['ios_tts'],
-	version=__version__,
+	version=version,
 	description='a pyttsx3 driver for ios device, it use AVFoundation.AVSpeechSynthesizer',
 	long_description='',
 	summary='pyttsx3 driver for ios device',
 	author='Yu Moqing',
 	url='https://github.com/yumoqing/ios_tts',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
 	author_email='yumoqing@gmail.com',
 	# install_requires=install_requires ,
 	keywords=['pyttsx' , 'ios', 'offline tts engine'],
 	classifiers = [
-		  'Intended Audience :: End Users/Desktop',
-		  'Intended Audience :: Developers',
-		  'Intended Audience :: Information Technology',
-		  'Intended Audience :: System Administrators',
-		  'Operating System :: android :: android TV',
-		  'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-		  'Programming Language :: Python :: 3'
+		'Intended Audience :: End Users/Desktop',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Information Technology',
+		'Intended Audience :: System Administrators',
+		'Operating System :: OS Independent',
+		'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+		'Programming Language :: Python :: 3'
 	],
 )
