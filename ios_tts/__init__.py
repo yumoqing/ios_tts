0fe31b4d6e87371dc2608e2f47ab4e9ca9d121b0
yumoqing <yumoqing@gmail.com>
2022-08-18 13:24:16 +0800

    bugfix

--- ios_tts/__init__.py
+++ ios_tts/__init__.py
@@ -144,7 +144,7 @@ class IOSSpeechDriver(BaseDriver):
 	def speak_sentence(self, sentence):
 		utterance = AVSpeechUtterance.alloc().init(sentence.text)
 		self.set_utterances_by_sentence(utterance, sentence)
-		self._tts.speak_(utterance)
+		self._tts.speak(utterance)
 
 	def getProperty(self, name):
 		if name == 'voices':
